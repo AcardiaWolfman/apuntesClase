19af91fdc5117528fe9a58c1a3e9f46666ae0872
AcardiaWolfman <oscarprograb@gmail.com>
2016-11-15 11:46:45 -0600

    Update archivos.c

--- archivos.c
+++ archivos.c
@@ -1,2 +1,30 @@
 #include<stdio.h>
-int main
+#include<stdlib.h>
+#include<time.h>
+int main(int argc,char **argv)
+{
+  /*
+  Funciones valudas para clase fopen fclose fread fwrite r r+ w w+ a a+
+  */
+  char tab[8][8];
+  FILE *fp=0;
+  fp=fopen("tablero.tab","r");
+  fp=fopen("tab.tab","w");
+  fwrite(tab,sizeof(char),64,fp);
+  
+  int n=fread(tab,sizeof(char),64,fp);
+  
+  
+  
+  
+  
+  fclose(fp);
+  
+  /*empieza aleatorios*/
+  time
+  stand(time(0));//seed
+  int volado=rand()%10;//random entre 0 y 9
+  
+  
+  usleep(1000000);
+}
